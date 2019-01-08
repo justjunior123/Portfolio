1631cf2ea30628f5189879e37ef5946ef2bcc0a6
justjunior89 <bballernumber5@sbcglobal.net>
2019-01-07 23:49:26 -0800

    add loader

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -2,15 +2,45 @@
 // critical: window.onload executes the script when the DOM is ready.
 //
 window.onload = () => {
+  const position = $('.load-screen').position();
+  const clone = $('.load-screen').clone();
 
-  var open = document.getElementById('button-toggle');
+
+  $('.load-screen').addClass('complete');
+  setTimeout(function(){
+    $('.load-screen').remove();
+  },3000);
+
+  const loader = () => { setTimeout($('.insert').append(clone),2000); };
+
+  let open = document.getElementById('button-toggle');
   open.addEventListener("click", function(){
     document.querySelector('.bg-modal').style.display = "flex";
   });
 
-  var close = document.querySelector('.close');
+  let close = document.querySelector('.close');
   close.addEventListener("click", function(){
     document.querySelector('.bg-modal').style.display = "none";
+  });
+
+  $(document).ready(function(){
+    $('body').on('click',function(e){
+      const target = $(e.target).attr('id');
+      console.log(target);
+      console.log(target === 'button-toggle');
+      if (target === 'load-chatter' || target === 'load-tetris'||
+       target === 'load-weather' || target === 'load-favorite'
+      ) {
+        loader();
+        setTimeout(1000);
+        scroll(0,position.top);
+        $('html, body').css({
+          'overflow': 'hidden',
+          'height': '100%'
+        });
+
+      }
+    });
   });
 
 };
